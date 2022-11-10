2a82bdbe6bf161ef69279eeaada09da8bf5dc392
marinagrebneva <sokovets@outlook.com>
2022-11-10 21:46:25 +0100

    added tests for the calculate bonus function

---
+++ calculateBonus.js
@@ -0,0 +1,19 @@
+const calculateBonus = (a, b) => {
+  let bonus;
+  const sum = a + b;
+  debugger;
+  //выполнение скрипта приостанавливается, показывается место остановки,
+  //все объявленные переменные и их значения на момент остановки отображаются слева
+  sum > 50 ? (bonus = 50) : (bonus = sum);
+  debugger; //значение в переменной bonus обновилось, остальные переменные остались прежними
+  return bonus;
+};
+
+// calculateBonus(5, 7)//необходимо вызвать функцию для демонстарации выражения debugger,
+// //иначе debugger не срабатывает т.к. расположен в функции
+// calculateBonus(45, 7)
+
+console.log(calculateBonus(-10, 7));
+console.log(calculateBonus(45, 7));
+
+module.exports = calculateBonus;
---
+++ calculateBonus.test.js
@@ -0,0 +1,30 @@
+const calculateBonus = require("./calculateBonus.js"); //импорт
+describe("Прямая проверка", () => {
+  it("Проверка выполнения условия, если сумма < 50", () => {
+    expect(calculateBonus(10, 5)).toBeLessThan(50);
+  });
+  it("Проверка выполнения условия, если сумма > 50", () => {
+    expect(calculateBonus(25, 60)).toBe(50);
+  });
+  it("Проверка выполнения условия, если сумма = 50", () => {
+    expect(calculateBonus(25, 25)).toBe(50);
+  });
+  it("Проверка выполнения условия, если сумма < 0", () => {
+    expect(calculateBonus(-20, 5)).toBeLessThan(0);
+  });
+  it("Проверка выполнения условия, если сумма = 0", () => {
+    expect(calculateBonus(-5, 5)).toBe(0);
+  });
+  it("Проверка выполнения условия, если сумма отицательное число", () => {
+    expect(calculateBonus(-30, -3)).toBeLessThan(50);
+  });
+});
+
+describe("Проверка граничных значений для нецелых чисел ", () => {
+  it("Сумма < 50", () => {
+    expect(calculateBonus(49, 0.55)).toBeLessThan(50);
+  });
+  it("Сумма > 50", () => {
+    expect(calculateBonus(49.55, 0.67)).toBeGreaterThanOrEqual(50);
+  });
+});
